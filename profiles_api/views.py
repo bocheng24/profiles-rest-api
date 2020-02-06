7154c1c244299dedf6ba838ecdd3ec10d1ac06d7
bocheng24 <jeffcgp@hotmail.com>
2020-02-06 15:36:18 +0800

    Add profile viewset with restricted permission

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permission
 
 
 class HelloApiView(APIView):
@@ -108,3 +110,10 @@ class HelloViewset(viewsets.ViewSet):
         return Response(
             {'http_method': 'DELETE'}
         )
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    '''Handle Creating and updating profiles'''
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permission.UpdateOwnProfile, )
